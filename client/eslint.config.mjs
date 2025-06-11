25a83928e7a746e3d7db3fc2d4fd60b9496f5091
ConnorWotkowicz <wottkowicz@gmail.com>
2025-06-11 15:58:43 -0400

    more and more syntax

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+const { dirname } = require("path");
+const { fileURLToPath } = require("url");
+const { FlatCompat } = require("@eslint/eslintrc");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  "plugin:@typescript-eslint/recommended",
+  "plugin:react/recommended", 
+  "plugin:react-hooks/recommended",
+  "plugin:jsx-a11y/recommended", 
+  "prettier", 
 ];
 
-export default eslintConfig;
+module.exports = eslintConfig;
